2e046ce604094b684ecc666b0e051ad5e521b79a
Donte Ladatto <donte279@gmail.com>
2022-03-02 15:59:11 -0600

    assignment submission

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    taskAdd(e.target.grabbable.value)
+  })
 });
+function taskAdd(todo) {
+  let li = document.createElement('li')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', deleteTime)
+  btn.textContent = "O"
+  li.textContent = `${todo} - `
+  li.appendChild(btn)
+  document.querySelector('#tasks').appendChild(li)
+}
+function deleteTime(e){
+  e.target.parentNode.remove();
+}
